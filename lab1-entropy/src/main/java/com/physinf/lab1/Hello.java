c7a5a85b6c74d314311c2671c299c1396ba550a7
Oleg <alig007@mail.ru>
2017-11-14 22:12:20 +0200

    Entropy calculator added. Lab task completed.

--- lab1-entropy/src/main/java/com/physinf/lab1/Hello.java
+++ lab1-entropy/src/main/java/com/physinf/lab1/Hello.java
@@ -5,29 +5,46 @@ import com.physinf.lab1.sequence.LogisticSequence;
 import com.physinf.lab1.sequence.SeqUtils;
 import com.physinf.lab1.sequence.StatisticUtils;
 
-import java.util.SortedMap;
+import java.util.Map;
 
 public class Hello {
 
+    // P that gives uniform distribution over binarized values
+    private static final double MU_UNIFORM = 3.846;
+
+    private static final double MU = 3.8;
+    private static final int N = 10_000;
+    private static final int MIN_LEN = 2;
+    private static final int MAX_LEN = 10;
+
     public static void main(String[] args) {
 
-        for (double mu = 0.5; mu <= 4.2; mu += 0.1) {
+        double[] values = SeqUtils.generate(new LogisticSequence(MU), N);
+        int[] binarized = SeqUtils.binarize(values, new LogisticBinarizer());
 
-            double[] values = SeqUtils.generate(new LogisticSequence(mu, 0.7692499279378784), 100_000);
+        for (int blockLen = MIN_LEN; blockLen <= MAX_LEN; blockLen++) {
+            int[] blocks = SeqUtils.makeBlocks(binarized, blockLen);
+
+            Map<Integer, Double> distribution = StatisticUtils.empiricDistribution(blocks);
+
+            double shannonEntropy = StatisticUtils.shannonEntropy(distribution);
+            double renyiEntropy2 = StatisticUtils.renyiEntropy(distribution, 2.0);
+            double renyiEntropy3 = StatisticUtils.renyiEntropy(distribution, 3.0);
+
+            System.out.printf("%d\t%f\t%f\t%f%n", blockLen, shannonEntropy, renyiEntropy2, renyiEntropy3);
+        }
+    }
+
+    private static void pOfMuTable() {
+        for (double mu = 3.5; mu <= 4.0; mu += 0.001) {
+
+            double[] values = SeqUtils.generate(new LogisticSequence(mu), 100_000);
             int[] binarized = SeqUtils.binarize(values, new LogisticBinarizer());
-
             int[] blocks = SeqUtils.makeBlocks(binarized, 1);
 
-        /*System.out.println(Arrays.toString(values));
-        System.out.println(Arrays.toString(binarized));*/
+            Map<Integer, Double> distribution = StatisticUtils.empiricDistribution(blocks);
 
-            System.out.println("mu = " + mu);
-
-            SortedMap<Integer, Double> distribution = StatisticUtils.empiricDistribution(blocks);
-            distribution.forEach((block, prob) -> {
-                System.out.println(Integer.toBinaryString(block) + " : " + prob);
-            });
-            System.out.println();
+            System.out.printf("%.4f\t%.7f%n", mu, distribution.getOrDefault(1, 0.0));
         }
     }
 }
--- lab1-entropy/src/main/java/com/physinf/lab1/sequence/StatisticUtils.java
+++ lab1-entropy/src/main/java/com/physinf/lab1/sequence/StatisticUtils.java
@@ -26,4 +26,24 @@ public abstract class StatisticUtils {
                 .mapValues(counter -> counter / (double) values.length)
                 .toSortedMap();
     }
+
+    public static double shannonEntropy(Map<Integer, Double> distribution) {
+        return distribution.values()
+                .stream()
+                .mapToDouble(p -> -p * log2(p))
+                .sum();
+    }
+
+    public static double renyiEntropy(Map<Integer, Double> distribution, double beta) {
+        double sum = distribution.values()
+                .stream()
+                .mapToDouble(p -> Math.pow(p, beta))
+                .sum();
+
+        return log2(sum) / (1.0 - beta);
+    }
+
+    private static double log2(double x) {
+        return Math.log(x) / Math.log(2);
+    }
 }
