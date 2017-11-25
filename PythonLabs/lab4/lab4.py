32a2348b7da1946aae9c868da951c270171c0d27
Oleg <alig007@mail.ru>
2017-11-25 22:30:46 +0200

    Phase volume plot added.

--- PythonLabs/lab4/lab4.py
+++ PythonLabs/lab4/lab4.py
@@ -60,30 +60,34 @@ def modelling(r, modelling_time=DEFAULT_TIME):
     for i in xrange(1, num_points):
         trace[i] = trace[i - 1] + STEP * lorenz.derivative(trace[i - 1])
 
-    return trace.T
+    return trace[:, 0], trace[:, 1], trace[:, 2]
 
 
-def attractor_for_various_r():
-    time = DEFAULT_TIME
+def time_values(modelling_time=DEFAULT_TIME):
+    num_points = int(modelling_time / STEP)
+    return np.linspace(0, modelling_time, num_points, True)
+
+
+def attractor_for_various_r(time=DEFAULT_TIME):
     fig = plt.figure("Lorenz Attractor, T = {}".format(time))
 
     r_midpoint = 1 + (R_CRITICAL - 1) / 2.0
 
     for i, r in enumerate([0.5, r_midpoint, R_CRITICAL + 1]):
-        points = modelling(r, time)
+        x, y, z = modelling(r, time)
 
         axes = fig.add_subplot(220 + i + 1, projection='3d')
         title = "R = {}".format(r)
 
         three_dim_plot(
             axes, title,
-            {'x': points[0, :], 'y': points[1, :], 'z': points[2, :]},
+            {'x': x, 'y': y, 'z': z},
             {'x': "X", 'y': "Y", 'z': "Z"}
         )
 
 
 def plot_attractor_with_projections(r, time=DEFAULT_TIME):
-    points = modelling(r, time)
+    x, y, z = modelling(r, time)
 
     fig = plt.figure("Attractor, R = {}, T = {}".format(r, time))
 
@@ -91,7 +95,7 @@ def plot_attractor_with_projections(r, time=DEFAULT_TIME):
     three_dim_plot(
         fig.add_subplot(131, projection='3d'),
         "3D view",
-        {'x': points[0, :], 'y': points[1, :], 'z': points[2, :]},
+        {'x': x, 'y': y, 'z': z},
         {'x': "X", 'y': "Y", 'z': "Z"}
     )
 
@@ -99,7 +103,7 @@ def plot_attractor_with_projections(r, time=DEFAULT_TIME):
     two_dim_plot(
         fig.add_subplot(132),
         "XY-projection",
-        {'x': points[0, :], 'y': points[1, :]},
+        {'x': x, 'y': y},
         {'x': "X", 'y': "Y"}
     )
 
@@ -107,13 +111,40 @@ def plot_attractor_with_projections(r, time=DEFAULT_TIME):
     two_dim_plot(
         fig.add_subplot(133),
         "XZ-projection",
-        {'x': points[0, :], 'y': points[2, :]},
+        {'x': x, 'y': z},
         {'x': "X", 'y': "Z"}
     )
 
 
+def plot_volumes(r, time=DEFAULT_TIME):
+    time_points = time_values(time)
+
+    # Real volume values
+    v_initial = INITIAL.prod()
+    real_phase_volume = v_initial * np.exp(-(S + 1 + B) * time_points)
+
+    # Modelled volume values
+    x, y, z = modelling(r, time)
+    calculated_phase_volume = x * y * z
+
+    # Plot on a separate plane
+    fig = plt.figure('Phase volume')
+    axes = fig.gca()
+
+    axes.set_title('Phase volume')
+    axes.set_xlabel('Time')
+    axes.set_ylabel('Volume')
+
+    axes.plot(time_points, calculated_phase_volume)
+    axes.plot(time_points, real_phase_volume)
+
+
 if __name__ == '__main__':
-    attractor_for_various_r()
-    plot_attractor_with_projections(R_CRITICAL + 2)
+    r = R_CRITICAL + 2
+    time = DEFAULT_TIME
+
+    attractor_for_various_r(time)
+    plot_attractor_with_projections(r, time)
+    plot_volumes(r, time)
 
     plt.show()
