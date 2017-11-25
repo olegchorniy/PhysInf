37834d279a38a3c748b93352179297822e0dc69a
Oleg <alig007@mail.ru>
2017-11-25 21:45:46 +0200

    Projection graphs  added.

--- PythonLabs/lab4/lab4.py
+++ PythonLabs/lab4/lab4.py
@@ -7,10 +7,30 @@ S = 10
 B = 8.0 / 3.0
 R = 28
 
+# Critical R
+R_CRITICAL = S * (S + B + 3.0) / (S - B - 1)
+
 # modelling related constants
-TIME = [20, 50, 250]
+DEFAULT_TIME = 50
 STEP = 1e-2
 INITIAL = np.array([2, -1, 0])
+
+
+# Plotting helpers
+
+def three_dim_plot(axes, title, points, labels):
+    axes.plot(points['x'], points['y'], points['z'])
+    axes.set_xlabel(labels['x'])
+    axes.set_ylabel(labels['y'])
+    axes.set_zlabel(labels['z'])
+    axes.set_title(title)
+
+
+def two_dim_plot(axes, title, points, labels):
+    axes.plot(points['x'], points['y'])
+    axes.set_xlabel(labels['x'])
+    axes.set_ylabel(labels['y'])
+    axes.set_title(title)
 
 
 class Lorenz(object):
@@ -30,14 +50,7 @@ class Lorenz(object):
         return dx_dt, dy_dt, dz_dt
 
 
-def critical_r():
-    r1 = 1  # constant
-    r2 = S * (S + B + 3.0) / (S - B - 1)
-
-    return r1, r2
-
-
-def modelling(modelling_time, r):
+def modelling(r, modelling_time=DEFAULT_TIME):
     lorenz = Lorenz(S, B, r)
     num_points = int(modelling_time / STEP)
 
@@ -50,24 +63,57 @@ def modelling(modelling_time, r):
     return trace.T
 
 
-def plot(title, plt_points):
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
+def attractor_for_various_r():
+    time = DEFAULT_TIME
+    fig = plt.figure("Lorenz Attractor, T = {}".format(time))
 
-    ax.plot(plt_points[0, :], plt_points[1, :], plt_points[2, :])
-    ax.set_xlabel("X Axis")
-    ax.set_ylabel("Y Axis")
-    ax.set_zlabel("Z Axis")
-    ax.set_title(title)
+    r_midpoint = 1 + (R_CRITICAL - 1) / 2.0
 
-    plt.show()
+    for i, r in enumerate([0.5, r_midpoint, R_CRITICAL + 1]):
+        points = modelling(r, time)
+
+        axes = fig.add_subplot(220 + i + 1, projection='3d')
+        title = "R = {}".format(r)
+
+        three_dim_plot(
+            axes, title,
+            {'x': points[0, :], 'y': points[1, :], 'z': points[2, :]},
+            {'x': "X", 'y': "Y", 'z': "Z"}
+        )
+
+
+def plot_attractor_with_projections(r, time=DEFAULT_TIME):
+    points = modelling(r, time)
+
+    fig = plt.figure("Attractor, R = {}, T = {}".format(r, time))
+
+    # Main plot
+    three_dim_plot(
+        fig.add_subplot(131, projection='3d'),
+        "3D view",
+        {'x': points[0, :], 'y': points[1, :], 'z': points[2, :]},
+        {'x': "X", 'y': "Y", 'z': "Z"}
+    )
+
+    # XY-projection
+    two_dim_plot(
+        fig.add_subplot(132),
+        "XY-projection",
+        {'x': points[0, :], 'y': points[1, :]},
+        {'x': "X", 'y': "Y"}
+    )
+
+    # XZ-projection
+    two_dim_plot(
+        fig.add_subplot(133),
+        "XZ-projection",
+        {'x': points[0, :], 'y': points[2, :]},
+        {'x': "X", 'y': "Z"}
+    )
 
 
 if __name__ == '__main__':
-    r = critical_r()[1]
-    points = modelling(TIME[2], r)
+    attractor_for_various_r()
+    plot_attractor_with_projections(R_CRITICAL + 2)
 
-    print "Critical R = {}".format(r)
-    print "Build plot from {} points".format(points)
-
-    plot("Lorenz Attractor", points)
+    plt.show()
